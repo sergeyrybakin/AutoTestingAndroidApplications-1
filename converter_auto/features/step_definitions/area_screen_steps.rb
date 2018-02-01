bbc460dcf5297cddf40a3673c7015b50c503a8c8
Sergey Rybakin <sergey.rybakin@gmail.com>
2018-02-01 22:33:35 +0700

    просмотрел 04 из 2го урока

--- converter_auto/features/step_definitions/area_screen_steps.rb
+++ converter_auto/features/step_definitions/area_screen_steps.rb
@@ -1,35 +1,49 @@
 Given(/^I land on help popup$/) do
-  puts("User lands on Help popup")
+  text("Помощь")
 end
 
 When(/^I click on Got it button$/) do
-  puts("User clicks on Got it button")
+  find_element(id:"button1").click
 end
 
 Then(/^I land on area screen$/) do
-  puts("User lands on Area screen")
+  text("Площадь")
 end
 
 When(/^I click on Swap button$/) do
-  puts("User clicks Swap button")
+  find_element(id:"fab").click
 end
 
 Then(/^I see "([^"]*)" in From header$/) do |value|
-  puts("From header values is " + value)
+  actual_value = find_element(id: "header_text_unit_from").text
+  puts("Expected value is #{value}")
+  puts("Actual value is #{actual_value}")
 end
 
 And(/^I see "([^"]*)" in To header$/) do |value|
-  puts("To header values is #{value}")
-end
+  actual_value = find_element(id: "header_text_unit_to").text
+  puts("Expected value is #{value}")
+  puts("Actual value is #{actual_value}")end
 
 And(/^I click on Clear button$/) do
-  puts("User clicks on Clear button")
+  find_element(id:"menu_clear").click
 end
 
 When(/^I enter "([^"]*)" to From field$/) do |value|
-  puts("User entered value #{value}")
+  find_element(id:"header_value_from").send_keys(value)
 end
 
 Then(/^I get "([^"]*)" in To field$/) do |value|
-  puts("User sees #{value} in result field")
+  actual_value = find_element(id: "header_value_to").text
+  puts("Expected value is #{value}")
+  puts("Actual value is #{actual_value}")
+end
+
+When(/^I click on From button$/) do
+  find_element(id:"header_value_from").click
+end
+
+And(/^I press "([^"]*)" on soft keyboard$/) do |value|
+  digit = Integer(value)
+  press_keycode 7 + digit
 end
