25b33898ecc5f0a565ee139d5a872192fc75b2ce
Sergey Rybakin <sergey.rybakin@gmail.com>
2018-02-09 22:39:54 +0700

    AutoTestingAndroidApplications-1 Ex3 Attempt-2

--- converter_auto/features/area_screen.feature
+++ converter_auto/features/area_screen.feature
@@ -2,20 +2,21 @@
 Feature: User is able to convert area units
 
   Background:
-    Given I click on "Закрыть" button
-    And I land on "Площадь" screen
+    Given I click on "Got It" button
+    And I land on "Area" screen
 
 #  Scenario: User is able to dismiss help
-#    Given I land on "Помощь" popup
-#    When I click on "Закрыть" button
-#    Then I land on "Площадь" screen
+#    Given I land on "Help" popup
+#    When I click on "Got It" button
+#    Then I land on "Area" screen
+
 
   Scenario: User is able to swap values
-    Given I see "Кв. километр" in From header
-    And I see "Кв. метр" in To header
+    Given I see "Sq Kilometre" in From header
+    And I see "Sq Metre" in To header
     When I click on Swap button
-    Then I see "Кв. метр" in From header
-    And I see "Кв. километр" in To header
+    Then I see "Sq Metre" in From header
+    And I see "Sq Kilometre" in To header
 
   Scenario Outline: User is able to convert default units
     Given I click on Clear button
@@ -24,24 +25,25 @@ Feature: User is able to convert area units
 
     Examples:
     |target|result|
-    |1|1.000.000|
-    |12|12.000.000|
-    |0.4|400.000|
+    |1|1000000|
+    |12|12000000|
+    |0.4|400000|
 
   Scenario: User is able to use soft keyboard to enter values
     Given I click on Clear button
     When I click on From button
-    And I press "13" on soft keyboard
-    Then I get "13.000.000" in To field
+    And I press "1.7" on soft keyboard
+    Then I get "1700000" in To field
 
   Scenario:
-    When I select "Гектар" from left column
-    Then I see "Гектар" in From header
-    And I get "10.000" in To field
+    When I select "Hectare" from left column
+    Then I see "Hectare" in From header
+    And I get "10000" in To field
 
+  @wip
   Scenario: User is able to see Help area
     Given I click on 3 vertical dots button
     And I land on FrameLayout screen
-    When I select "Помощь" on FrameLayout screen
-    Then I see "Помощь" on FrameLayout screen
-    And I click on "Закрыть" button
+    When I select "Help" on FrameLayout screen
+    Then I see "Help" on FrameLayout screen
+    And I click on "Got It" button
--- converter_auto/features/step_definitions/area_screen_steps.rb
+++ converter_auto/features/step_definitions/area_screen_steps.rb
@@ -1,19 +1,12 @@
 Given(/^I land on "([^"]*)" popup$/) do |value|
-  popup = text("#{value}")
-  popup.wont_be_empty
+  text("#{value}")
 end
 
 When(/^I click on "([^"]*)" button$/) do |value|
-# working string  find_element(id: "buttonPanel").find_element(xpath: "//android.widget.Button[@text='#{value}']").click
-  button = (find_element(id: "buttonPanel").find_element(xpath: "//android.widget.Button[@text='#{value}']"))
-  if button.enabled? != true
-    fail("Button #{value} should be enabled")
-  end
-  button.click
+  find_element(id: "buttonPanel").find_element(xpath: "//android.widget.Button[@text='#{value}']").click
 end
 
 Then(/^I land on "([^"]*)" screen$/) do |value|
-# working string    find_element(id: "toolbar").find_element(xpath: "//android.widget.TextView[@text='#{value}']")
   actual_value = find_element(id: "toolbar").find_element(xpath: "//android.widget.TextView[@text='#{value}']").text
   if actual_value != value
     fail("Expected value is #{value}, but actual value is #{actual_value}")
@@ -21,12 +14,7 @@ Then(/^I land on "([^"]*)" screen$/) do |value|
 end
 
 When(/^I click on Swap button$/) do
-# working string      find_element(id:"fab").click
-  button = find_element(id:"fab")
-  if button.enabled? != true
-    fail("Button Swap should be enabled")
-  end
-  button.click
+  find_element(id:"fab").click
 end
 
 Then(/^I see "([^"]*)" in From header$/) do |value|
@@ -44,7 +32,6 @@ And(/^I see "([^"]*)" in To header$/) do |value|
 end
 
 And(/^I click on Clear button$/) do
-# working string        find_element(id:"menu_clear").click
   button = find_element(id:"menu_clear")
   if button.enabled? != true
     fail("Button Clear should be enabled")
@@ -53,12 +40,7 @@ And(/^I click on Clear button$/) do
 end
 
 When(/^I enter "([^"]*)" to From field$/) do |value|
-#working str  find_element(id:"header_value_from").send_keys(value)
-  input_field = find_element(id:"header_value_from")
-  if input_field.enabled? != true
-    fail("Input field 'From' should be enabled")
-  end
-  input_field.send_keys(value)
+  find_element(id:"header_value_from").send_keys(value)
 end
 
 Then(/^I get "([^"]*)" in To field$/) do |value|
@@ -69,12 +51,7 @@ Then(/^I get "([^"]*)" in To field$/) do |value|
 end
 
 When(/^I click on From button$/) do
-#working str  find_element(id:"header_value_from").click
-  input_field = find_element(id:"header_value_from")
-  if input_field.enabled? != true
-    fail("Input field 'From' should be enabled")
-  end
-  input_field.click
+  find_element(id:"header_value_from").click
 end
 
 And(/^I press "([^"]*)" on soft keyboard$/) do |value|
@@ -90,21 +67,11 @@ And(/^I press "([^"]*)" on soft keyboard$/) do |value|
 end
 
 When(/^I select "([^"]*)" from left column$/) do |value|
-#working str    find_element(id: "radio_group_from").find_element(xpath: "//android.widget.RadioButton[@text='#{value}']").click
-  input_field = find_element(id:"radio_group_from").find_element(xpath: "//android.widget.RadioButton[@text='#{value}']")
-  if input_field.enabled? != true
-    fail("Input field RadioButton #{value} should be enabled")
-  end
-  input_field.click
+  find_element(id: "radio_group_from").find_element(xpath: "//android.widget.RadioButton[@text='#{value}']").click
 end
 
 Given(/^I click on 3 vertical dots button$/) do
-#working str  find_element(id: "toolbar").find_element(xpath: "//android.widget.ImageView").click
-  button = find_element(id: "toolbar").find_element(xpath: "//android.widget.ImageView")
-  if button.enabled? != true
-    fail("Button 3 vertical dots should be enabled")
-  end
-  button.click
+  find_element(id: "toolbar").find_element(xpath: "//android.widget.ImageView").click
 end
 
 And(/^I land on FrameLayout screen$/) do
@@ -116,13 +83,11 @@ end
 
 When(/^I select "([^"]*)" on FrameLayout screen$/) do |value|
   (find_element(xpath: "//android.widget.FrameLayout[1]").find_element(xpath: "//android.widget.ListView[1]").find_element(xpath: "//android.widget.TextView[@text='#{value}']")).click
-#  Почему-то не получилась проверка:
-#  if button.enabled? != true
-#    fail("Button #{value} should be enabled")
-#  end
-#  button.click
 end
 
 Then (/^I see "([^"]*)" on FrameLayout screen$/) do |value|
-  find_element(xpath: "//android.widget.FrameLayout[1]").find_element(id: "parentPanel").find_element(xpath: "//android.widget.TextView[@text='#{value}']")
+  actual_value = find_element(id: "topPanel").find_element(xpath: "//android.widget.TextView[@text='#{value}']").text
+  if actual_value != value
+    fail("Expected value is #{value}, but actual value is #{actual_value}")
+  end
 end
